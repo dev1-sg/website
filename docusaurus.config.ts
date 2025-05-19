ae2de36b20e49bead693e674de622ff542de5de3
antyung47 <antyung47@gmail.com>
2025-05-19 14:05:40 +0000

    project update

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -10,7 +10,7 @@ const config: Config = {
   favicon: 'img/favicon.ico',
 
   // Set the production url of your site here
-  url: 'https://dev1-sg.github.io/website/',
+  url: 'https://dev1-sg.github.io/',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
   baseUrl: '/website/',
