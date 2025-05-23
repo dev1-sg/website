bf63fcd29b925f7a2d3466eea31ea1d1950ea79f
antyung47 <antyung47@gmail.com>
2025-05-23 16:59:54 +0000

    project update

--- website/docusaurus.config.js
+++ website/docusaurus.config.js
@@ -8,7 +8,7 @@ import {themes as prismThemes} from 'prism-react-renderer';
 
 /** @type {import('@docusaurus/types').Config} */
 const config = {
-  // title: '',
+  title: 'dev1.sg',
   // tagline: '',
   favicon: 'img/favicon.ico',
 
